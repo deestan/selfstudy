2fdeaf67a9f4b0f70f535686723c874ae68d6634
Helge Skogly Holm <helge.holm@gmail.com>
2015-05-25 12:49:58 +0200

    Serves questions as JSON on /api/questions

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 var express = require("express");
 var app = express();
+
+var questions = require("./questions");
+
 var PORT = 8080;
 
 app.use(express.static(__dirname + "/static"));
+
+app.get('/api/questions', function(req, res) {
+  res.json(questions);
+});
+
 app.listen(PORT);
 console.log("Server running at:");
 console.log("http://localhost:" + PORT + "/");
