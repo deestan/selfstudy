d4b357effa2a544d12887e2b0154835dbcf09215
Helge Skogly Holm <helge.holm@gmail.com>
2015-05-25 13:20:13 +0200

    Use randy for randomization on fetch test page.

--- static/js/fetch.js
+++ static/js/fetch.js
@@ -1,7 +1,6 @@
 function nextQuestion() {
   $.getJSON('/api/questions', function(data) {
-    var idx = Date.now() % data.length;
-    $("#question").text(JSON.stringify(data[idx]));
+    $("#question").text(JSON.stringify(randy.choice(data)));
   });
 }
 
