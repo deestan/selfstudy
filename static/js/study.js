e95cd2c0d645a847c65ba5bb69411bca0741e241
Helge Skogly Holm <helge.holm@gmail.com>
2015-05-25 14:05:11 +0200

    Wired up yay and nay buttons.

--- static/js/study.js
+++ static/js/study.js
@@ -3,8 +3,18 @@ $(init);
 function init() {
   console.log('inited');
   $(".btn-go").on('click', reveal);
+  $(".yay .btn").on('click', yay);
+  $(".nay .btn").on('click', nay);
 }
 
 function reveal() {
   $('.exercise').addClass('reveal');
+}
+
+function yay() {
+  $('.exercise').removeClass('reveal');
+}
+
+function nay() {
+  $('.exercise').removeClass('reveal');
 }
