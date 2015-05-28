2d84eb9c305c3539c50699f53ccf4ffa2724017a
Helge Skogly Holm <helge.holm@gmail.com>
2015-05-28 18:46:13 +0200

    Added rudimentary login stuff.

--- server.js
+++ server.js
@@ -1,17 +1,35 @@
 var express = require("express");
-var app = express();
+var session = require("express-session");
+var passport = require("passport");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+var flash = require("connect-flash");
 
 var exercises = require("./exercises");
 
 var PORT = 8080;
 var serverStartTime = Date.now();
+var app = express();
+require('./config-passport')(passport);
 
+app.use(cookieParser());
+app.use(bodyParser());
+app.use(session({ secret: "headbunny" }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
 app.use(express.static(__dirname + "/static"));
 
 app.get('/api/exercises', function(req, res) {
   res.json({ exercises: exercises,
              version: serverStartTime });
 });
+
+app.post('/login', passport.authenticate('local-login', {
+  successRedirect: '/',
+  failureRedirect: '/login.html',
+  failureFlash: true
+}));
 
 app.listen(PORT);
 console.log("Server running at:");
