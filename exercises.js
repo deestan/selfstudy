7414db0b910b62d9f83357974d6e9c6d2ca0ac15
Helge Skogly Holm <helge.holm@gmail.com>
2015-06-20 16:04:37 +0200

    Added exercises for a part 2.

--- exercises.js
+++ exercises.js
@@ -20,5 +20,24 @@ module.exports = {
       "a": "<code>&lt;body&gt;</code>" },
     { "q": "<p>What differs HyperText from just text?</p>",
       "a": "HyperText allows links to let you move from on page to another." }
+  ],
+  
+  "HTML & CSS part 2": [
+    { "q": "Which tag do you use to markup the main heading on a page?",
+      "a": "&lt;h1&gt;" },
+    { "q": "How many levels of headings does HTML have?",
+      "a": "6" },
+    { "q": "Words wrapped in an opening &lt;p&gt; and a closing &lt;/p&gt; creates...",
+      "a": "A paragraph." },
+    { "q": "Does the browser display two or more spaces next to each other?",
+      "a": "No, it displays only one space.  This is called white space collapsing." },
+    { "q": "How do you create a line break inside a paragraph?",
+      "a": "By using the &lt;br&gt; element." },
+    { "q": "Which of these tags provide structure, and which provide semantics? &lt;h1&gt; &lt;blockquote&gt; &lt;hr&gt; &lt;strong&gt; &lt;p&gt; &lt;em&gt; &lt;h6&gt; &lt;q&gt;",
+      "a": "Structural: &lt;h1&gt; &lt;h6&gt; &lt;hr&gt; &lt;p&gt;, Semantic: &lt;blockquote&gt; &lt;strong&gt; &lt;em&gt; &lt;q&gt;" },
+    { "q": "How can you add a semantic element to explain the abbreviation &lt;p&gt;HTML&lt;/p&gt;?",
+      "a": "<code>&lt;p&gt;&lt;abbr title=\"HyperText Markup Language\"&gt;HTML&lt;/abbr&gt;&lt;/p&gt;<code>" },
+    { "q": "How would you write H<sub>2</sub>O in HTML?",
+      "a": "H&lt;sub&gt;2&lt;/sub&gt;O" }
   ]
 }
